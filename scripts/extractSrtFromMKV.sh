a02e498ee0e703821bb04d774fe126d194905c39
Tree Ant <treeantsan@gmail.com>
2022-09-10 22:19:18 -0400

    renaming scripts to be more sensible

--- scripts/extractSrtFromMKV.sh
+++ scripts/extractSrtFromMKV.sh
@@ -7,12 +7,15 @@
 # Then add this script to your PATH so you can run it from anywhere. Or make
 # it an alias if you don't wanna do that.
 
-# Do note that here I'm assuming the subtitle track is track 2
+# Do note that here I'm assuming the subtitle track is track 1
+# You can discover which track is the subtitle track by running the following:
+# mkvmerge -i <filename>.mkv
+
 
 
 for i in *.mkv; do
     if [ ! -e "${i%.*}".srt ]; then
         echo "${i%.*} ok"
-        mkvextract tracks "$i" 2:"${i%.*}".srt
+        mkvextract tracks "$i" 1:"${i%.*}".srt
     fi
 done
